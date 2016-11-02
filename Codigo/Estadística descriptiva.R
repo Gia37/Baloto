aeec7f64e1e9e9bc85998e2ea30b32e2b79ec45f
Gia37 <giselaarrietarivera@gmail.com>
2016-11-02 01:14:35 -0500

    corrigiendo y agregando lineas a la estadistica

--- Codigo/Estadística descriptiva.R
+++ Codigo/Estadística descriptiva.R
@@ -1,21 +1,100 @@
 #Importando la tabla desde el archivo Datos.sh
-tabla = read.table("Frecuencia.sh")
+tabla = read.table("Datos.dat")
 
-#Suma de todos los valores de V1 hasta V6
-Vec = c(tabla$V2)
+#------------------------------------------------------------
+# suma de los datos historicos de cada una de las balotas
+#------------------------------------------------------------
+v1 = c(tabla$V1) 
+v2 = c(tabla$V2)
+v3 = c(tabla$V3)
+v4 = c(tabla$V4)
+v5 = c(tabla$V5)
+v6 = c(tabla$V6)
 
-#Calcula la Media
-Media = "Media: "; Media; mean(Vec)
+#------------------------------------------------------------
+#Calcula la Media de cada balota
+#------------------------------------------------------------
 
-#Calcula la Desviacion estandar
-Desviación = "Desviación estandar: "; Desviación; sd(Vec)
+#Calcula la Media de V1, es decir la media de la balota 1
+Media = "Media de la primera balota"; Media; mean(v1)
 
-#Calcula el minimo
-Mínimo ="Mínimo: "; Mínimo; min(Vec) 
+#Calcula la Media de V2, es decir la media de la balota 2
+Media = "Media de la segunda balota"; Media; mean(v2)
 
-#Calcula el maximo
-Máximo = "Máximo: "; Máximo; max(Vec)
+#Calcula la Media de V3, es decir la media de la balota 3
+Media = "Media de la tercera balota"; Media; mean(v3)
 
-# Grafico de estadistica de numeros
-barplot(tabla$V2, main="Estadística de números", xlab="Balotas", col="lightblue")
+#Calcula la Media de V4, es decir la media de la balota 4
+Media = "Media de la cuarta balota"; Media; mean(v4)
 
+#Calcula la Media de V5, es decir la media de la balota 5
+Media = "Media de la quinta balota"; Media; mean(v5)
+
+#Calcula la Media de V6, es decir la media de la balota 6
+Media = "Media de la sexta balota"; Media; mean(v6)
+
+#------------------------------------------------------------
+#Calcula la Desviacion Estandar de cada balota
+#------------------------------------------------------------
+
+#Calcula la Desviacion estandar de la balota 1
+Desviación = "Desviación estandar de la primea balota"; Desviación; sd(v1)
+
+#Calcula la Desviacion estandar de la balota 2
+Desviación = "Desviación estandar de la segunda balota"; Desviación; sd(v2)
+
+#Calcula la Desviacion estandar de la balota 3
+Desviación = "Desviación estandar de la tercera balota"; Desviación; sd(v3)
+
+#Calcula la Desviacion estandar de la balota 4
+Desviación = "Desviación estandar de la cuarta balota"; Desviación; sd(v4)
+
+#Calcula la Desviacion estandar de la balota 5
+Desviación = "Desviación estandar de la quinta balota"; Desviación; sd(v5)
+
+#Calcula la Desviacion estandar de la balota 6
+Desviación = "Desviación estandar de la sexta balota"; Desviación; sd(v6)
+
+#------------------------------------------------------------
+#Calcula el valor minimo de cada balota
+#------------------------------------------------------------
+
+#Calcula el valor minimo de la balota 1
+Mínimo ="Mínimo de la primera balota"; Mínimo; min(v1) 
+
+#Calcula el valor minimo de la balota 2
+Mínimo ="Mínimo de la segunda balota"; Mínimo; min(v2)
+
+#Calcula el valor minimo de la balota 3
+Mínimo ="Mínimo de la tercera balota"; Mínimo; min(v3)
+
+#Calcula el valor minimo de la balota 4
+Mínimo ="Mínimo de la cuarta balota"; Mínimo; min(v4)
+
+#Calcula el valor minimo de la balota 5
+Mínimo ="Mínimo de la quinta balota"; Mínimo; min(v5)
+
+#Calcula el valor minimo de la balota 6
+Mínimo ="Mínimo de la sexta balota"; Mínimo; min(v6)
+
+#------------------------------------------------------------
+#Calcula el valor maximo de cada balota
+#------------------------------------------------------------
+
+#Calcula el valor maximo de la balota 1
+Máximo = "Máximo de la primera balota"; Máximo; max(v1)
+
+#Calcula el valor maximo de la balota 2
+Máximo = "Máximo de la segunda balota"; Máximo; max(v2)
+
+#Calcula el valor maximo de la balota 3
+Máximo = "Máximo de la tercera balota"; Máximo; max(v3)
+
+#Calcula el valor maximo de la balota 4
+Máximo = "Máximo de la cuarta balota"; Máximo; max(v4)
+
+#Calcula el valor maximo de la balota 5
+Máximo = "Máximo de la quinta balota"; Máximo; max(v5)
+
+#Calcula el valor maximo de la balota 6
+Máximo = "Máximo de la sexta balota"; Máximo; max(v6)

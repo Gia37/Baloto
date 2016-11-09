d9b5a11e33a0f5ed15554548c691ee3f2d266120
eilinluna16 <eipalumor12@hotmail.com>
2016-11-08 20:03:18 -0500

    Se agregan errores y excepciones al código para un mejor ingreso de los datos

--- Codigo/Consulta.py
+++ Codigo/Consulta.py
@@ -15,6 +15,18 @@ def Datos():
         Sorteo2 += [Lista[4]]
     h.close()
     
+    Serie.remove(':---:')
+    Serie.remove(' SERIE ')
+    Fecha.remove(':---:')
+    Fecha.remove(' FECHA ')
+    Sorteo1.remove(':---:')
+    Sorteo1.remove(' BALOTO ')
+    Sorteo2.remove(':---:')
+    Sorteo2.remove(' REVANCHA ')
+
+    for i in range(len(Serie)):
+        Serie[i] = int(Serie[i])
+        
     m = open('Frecuencia.dat', 'r')
     lines = m.readlines()
     Numero, Frecuencia = [], []
@@ -35,17 +47,35 @@ def Consulta():
     print "2. Sorteos que han salido en el a?o "
     print "3. Sorteos que han salido en la fecha "
     print "4. Cuantas veces ha salido el numero  "
-    resp = input()
-    
+    print "5. SALIR"    
+    #--------------------------------------------------------------------------
+    resp = raw_input(">> ")
+    while type(resp) != int:
+        try:
+            resp = int(resp)
+        except:    
+            resp = raw_input("ERROR: Ingrese, 1, 2, 3, 4, o 5 >> ")
+    #--------------------------------------------------------------------------
     if resp==1:
-        A =raw_input("Ingrese el numero de serie ")
+        A =raw_input("Ingrese el numero de serie (1 hasta "+str(max(S))+")>> ")
+        while type(A)!=int:
+            try:
+                A = int(A)
+            except:
+                A = raw_input("Ingrese numero de 1 a "+str(max(S))+" >> ")
         for i in range(2, len(S)):
-            if int(S[i])==int(A):
+            if S[i]==A:
                 return 1, [S1[i], S2[i]]
         return 1, "ERROR: Dato no encontrado."
+    #--------------------------------------------------------------------------
     elif resp==2:
-        A =raw_input("Ingrese el a?o AAAA (2001 hasta el 2016)")
-        AA = int(A) - 2000
+        A =raw_input("Ingrese el a?o AAAA (2001 hasta el 2016) >> ")
+        while type(A)!=int:
+            try:
+                A = int(A)
+            except:
+                A = raw_input("Ingrese a?o de 2001 a 2016 >> ")
+        AA = A- 2000
         x = []
         for i in range(2, len(F)):
             F[i] = F[i].split('/')
@@ -55,18 +85,33 @@ def Consulta():
             return 2, x
         else:
             return 2, "ERROR: Dato no encontrado."
+    #--------------------------------------------------------------------------
     elif resp==3:
-        A =raw_input("Ingrese la fecha DD/MM/AA ")
+        A =raw_input("Ingrese la fecha DD/MM/AA >> ")
+        while ' ' in list(A):
+            A = list(A)
+            A.remove(' ')
+            A = ''.join(A)
         for i in range(2, len(S)):
             if F[i]== " "+A+" ":
                 return 3, [S1[i], S2[i]]
         return 3, "ERROR: Dato no encontrado."
+    #--------------------------------------------------------------------------
     elif resp==4:
-        A =raw_input("Ingrese numero de sorteo (1 hasta 45) ")
+        A =raw_input("Ingrese numero de sorteo (1 hasta 45) >> ")
+        while type(A)!=int:
+            try:
+                A = int(A)
+            except:
+                A = raw_input("Ingrese numero de 1 hasta 45 >> ")
         for i in range(45):
-            if int(A)==int(Num[i]):
+            if A==int(Num[i]):
                 return 4, [Num[i], Frec[i]]
         return 4, "ERROR: Dato no encontrado."
+    #--------------------------------------------------------------------------
+    elif resp==5:
+        return 5, "PROCESO TERMINADO"
+    #--------------------------------------------------------------------------
     else: 
         return 0, "ERROR: Escoja una de las opciones: 1, 2, 3 o 4"
         
